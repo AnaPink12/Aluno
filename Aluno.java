a966cd153fbc04acfe5c2c0ae7956bf9400c1a40
AnaPink12 <124005580+AnaPink12@users.noreply.github.com>
2023-03-05 17:29:50 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,9 @@
+class Aluno {
+    Public String protegido nomeCompleto;
+    protected int idade;
+
+    public Aluno(String nomeCompleto, int idade) {
+        isso.nomeCompleto = nomeCompleto;
+        isso.idade = idade;
+    }
+}
